0af9eaf858bdf0dd7ef7a76c727b48d78bf38293
Adrian <adrianhenry2115@gmail.com>
2023-09-28 21:01:58 -0400

    navbar complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchInput from "./inputs/SearchInput";
 import LoginBtn from "./buttons/LoginBtn";
 import SignupBtn from "./buttons/SignupBtn";
-import Dropdown from "./dropdowns/Dropdown";
 import { PiFilmSlateLight } from "react-icons/pi";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
     return (
-        <nav className="flex flex-col py-6 px-10 w-full shadow-lg bg-black bg-opacity-10">
+        <nav className="flex flex-col py-6 px-10 w-full shadow-lg bg-black bg-opacity-10 sticky">
             <div className="flex flex-col lg:flex-row lg:justify-center justify-evenly items-center">
                 {/* LOGO AND NAME */}
                 <div className="flex items-center pb-6">
@@ -19,33 +18,36 @@ const Navbar = () => {
 
                     {/* PAGE NAVIGATION  */}
                     <div className="md:flex md:items-center hidden">
-                        <span className="page-nav">Home</span>
-                        <span className="page-nav">New</span>
-                        <span className="page-nav">Trending</span>
-                        <span className="page-nav">Watchlist</span>
-                        <span className="page-nav">Review</span>
+                        <a href="/" className="page-nav">
+                            Home{" "}
+                        </a>
+                        <a href="/new" className="page-nav">
+                            New
+                        </a>
+                        <a href="trending" className="page-nav">
+                            Trending
+                        </a>
+                        <a href="watchlist" className="page-nav">
+                            Watchlist
+                        </a>
+                        <a href="review" className="page-nav">
+                            Review
+                        </a>
                     </div>
+                    {/* HAMBURGER FOR SMALL SCREENS */}
                     <div className="lg:hidden flex cursor-pointer right-0 absolute pr-6">
                         <RxHamburgerMenu size={20} />
                     </div>
                 </div>
-                {/* SEARCH INPUT */}
-                {/* LOG IN BUTTON/HAMBURGER  */}
                 <div className="hidden lg:flex right-0 absolute left-auto pr-4">
                     <LoginBtn text="Log In" onClick={() => {}} />
                     {/* SIGN UP BUTTON  */}
                     <SignupBtn text="Sign Up" onClick={() => {}} />
                 </div>
             </div>
+            {/* SEARCH INPUT  */}
             <div className="flex mr-10 w-full justify-center items-center">
                 <SearchInput />
-            </div>
-            {/* STREAMING SERVICES DROPDOWNS -- NETFLIX | AMAZON | HULU | MORE */}
-            <div className="md:flex md:justify-center hidden">
-                <Dropdown text="Netflix" />
-                <Dropdown text="Amazon" />
-                <Dropdown text="Hulu" />
-                <Dropdown text="More" />
             </div>
         </nav>
     );
--- src/components/inputs/SearchInput.tsx
+++ src/components/inputs/SearchInput.tsx
@@ -8,7 +8,7 @@ const SearchInput = () => {
                 className="flex border-black border-y-2 border-l-2 px-4 py-2 rounded-s-sm resize lg:w-96 w-72"
                 placeholder="Inception, Breaking Bad, Westworld"
             />
-            <div className="bg-black text-white rounded-e-md" style={{ padding: "14px" }}>
+            <div className="bg-black text-white rounded-e-md cursor-pointer" style={{ padding: "14px" }}>
                 <BsSearch />
             </div>
         </div>
